ce451c483f7c195813c9aa48f29f7729d9354747
Rumi <jun32820@gmail.com>
2025-02-11 19:39:50 +0900

    Update FCRUtility.cs

--- FCRUtility.cs
+++ FCRUtility.cs
@@ -4,6 +4,6 @@ namespace Rumi.FixCameraResolutions
 {
     public static class FCRUtility
     {
-        public static Vector2 Multiply(this Vector2 position, float x, float y) => new Vector2(position.x * x, position.y * y);
+        public static Vector2 Multiply(this Vector2 position, float x, float y) => new(position.x * x, position.y * y);
     }
 }
